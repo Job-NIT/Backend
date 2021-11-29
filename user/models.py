b997a5a38ed8dc546780eb314450b8961793d0e7
SayeMarg <sayemarg.developer@gmail.com>
2021-11-29 21:32:25 +0330

    Create Simple Freelancer, Employer Model

--- user/models.py
+++ user/models.py
@@ -1,10 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(AbstractUser):
+    class USER_TYPE(models.TextChoices):
+        FREELANCER = 'Freelancer'
+        EMPLOYER = 'Employer'
+
     email = models.EmailField(unique=True, blank=False)
     phone_number = models.CharField(max_length=12, default='')
+    user_type = models.CharField(
+        max_length=15,
+        choices=USER_TYPE.choices,
+        default=USER_TYPE.FREELANCER,
+        blank=False
+    )
 
     def __str__(self):
         return self.username
+
+
+class Employer(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+    company = models.CharField(max_length=255, blank=True)
+
+
+class Freelancer(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
