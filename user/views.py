02e53d883220ec7541b0f3383e4143d4771dabb5
SayeMarg <sayemarg.developer@gmail.com>
2021-12-01 15:41:13 +0330

    Create Login, Logout With Token Authentication

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_204_NO_CONTENT
 
-# Create your views here.
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        request.auth.delete()
+
+        return Response(status=HTTP_204_NO_CONTENT)
