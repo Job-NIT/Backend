b4439a2a83d295ced3d47fe3628142f5a6e75ad3
SayeMarg <sayemarg.developer@gmail.com>
2021-12-02 22:24:04 +0330

    Register Freelancer, Employer API

--- user/views.py
+++ user/views.py
@@ -1,8 +1,20 @@
-from rest_framework import status
+from .models import Freelancer, Employer
+from .serializers import FreelancerSerializer, EmployerSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.generics import CreateAPIView
+
+
+class FreelancerRegisterView(CreateAPIView):
+    serializer_class = FreelancerSerializer
+    queryset = Freelancer.objects.all()
+
+
+class EmployerRegisterView(CreateAPIView):
+    serializer_class = EmployerSerializer
+    queryset = Employer.objects.all()
 
 
 class LogoutView(APIView):
