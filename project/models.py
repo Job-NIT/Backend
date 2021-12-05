4088cfd0e39ee9996911a541da849c6bb19c385c
SayeMarg <sayemarg.developer@gmail.com>
2021-12-05 20:44:09 +0330

    Create Simple Project Model

---
+++ project/models.py
@@ -0,0 +1,14 @@
+from user.models import Employer
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    detail = models.TextField()
+    dead_line = models.DateTimeField()
+    budget = models.PositiveIntegerField(default=0)
+    employer = models.ForeignKey(
+        Employer,
+        on_delete=models.CASCADE,
+        related_name='projects'
+    )
